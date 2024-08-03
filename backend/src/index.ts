1dd113111f021989201b9980c8f9016cdbf648e5
manavnotop <manuagarwal1443@gmail.com>
2024-08-03 14:41:19 +0530

    add sign up route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,49 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL : string,
+    JWT_SECRET: string,
+  }
+}>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post('/api/v1/user/signup', async (c) => {
+  const prisma = new PrismaClient({datasourceUrl: c.env.DATABASE_URL,}).$extends(withAccelerate())
+  const body = await c.req.json();
+  try{
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text("post signup");
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: body.email
+      }
+    })
+
+    if(existingUser){
+      return c.json({
+        message: "Users already exists"
+      }, 400)
+    }
+
+    const newUser = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.email,
+        name: body.name,
+      }
+    })
+
+    const jwt = await sign({id: newUser.id}, c.env.JWT_SECRET);
+
+    return c.json(jwt);
+  }
+  catch(e){
+    console.error(e);
+    c.status(403);
+    return c.json("Error while signing up");
+  }
 })
 
 app.post('/api/v1/user/signin', (c) => {
